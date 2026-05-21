3a95b92a8ae2021c1d2375be01ab5a6c444b37de
MajitoLR <larreamariajose456@gmail.com>
2026-05-20 20:16:19 -0500

    Implement Flask API for temperature data

--- backend/sensor_api.py
+++ backend/sensor_api.py
@@ -1,1 +1,26 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+datos_sensor = {
+    "temperatura": 0
+}
+
+@app.route('/temperature', methods=['POST'])
+def recibir_temperatura():
+
+    data = request.json
+
+    datos_sensor["temperatura"] = data["temp"]
+
+    return jsonify({
+        "mensaje": "Temperatura recibida"
+    })
+
+@app.route('/temperature', methods=['GET'])
+def obtener_temperatura():
+
+    return jsonify(datos_sensor)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
